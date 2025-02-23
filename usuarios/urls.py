748adc80b314cff7c6b55255ee8719de50493fcb
Cristian Rivera <107967772+cristianriveraxd@users.noreply.github.com>
2025-02-22 20:57:20 -0500

    Update urls.py

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .routers import UsuariosViewSet, ProductosViewSet
+from .routers import *
+from .views import *
 
 router = DefaultRouter()
 router.register(r'usuarios', UsuariosViewSet)
 router.register(r'productos', ProductosViewSet)
+router.register(r'usuarios-jd', UsuarioViewSet)
+router.register(r'pedidos-jd', PedidoViewSet)
+router.register(r'perfil-jp', PerfilViewSet)
+router.register(r'clientes-r', ClienteViewSet)
+
 
 urlpatterns = [
     path('', include(router.urls)),
