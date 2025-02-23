1d24fc23031fda0e4e5b7988833f8ae3afede030
Cristian Rivera <107967772+cristianriveraxd@users.noreply.github.com>
2025-02-22 20:36:59 -0500

    Update serializers.py

--- usuarios/serializers.py
+++ usuarios/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Usuarios, Productos
+from .models import Usuarios, Productos, UsuariosJD, PedidoJD, PerfilJorge, UsuarioJorge, ClienteRocha
 
 class ProductosSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,4 +10,34 @@ class UsuariosSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Usuarios
+        fields = '_all_'
+
+class UsuariosJDSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UsuariosJD
+        fields = '_all_'
+
+class PedidoJDSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PedidoJD
+        fields = '_all_'
+
+class PerfilJorgeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PerfilJorge
+        fields = '_all_'
+
+class UsuarioJorgeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UsuarioJorge
+        fields = '_all_'
+
+class ClienteRochaJorgeSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ClienteRocha
         fields = '_all_'
